f61d582536ca1f1ff86d42bdb3ae95f02cf09775
christianreiser <reisercg@gmail.com>
2020-06-23 21:53:16 +0200

    import_from_json_route.dart works

--- lib/Import/import_from_json_route.dart
+++ lib/Import/import_from_json_route.dart
@@ -10,8 +10,6 @@ import 'package:file_picker/file_picker.dart';
 import 'package:insightme/Database/database_helper_entry.dart';
 import '../Database/entry.dart';
 
-import 'package:path_provider/path_provider.dart';
-
 class Import extends StatefulWidget {
   @override
   _ImportState createState() => _ImportState();
@@ -21,7 +19,8 @@ class _ImportState extends State<Import> {
   final DatabaseHelperEntry helperEntry = // error when static
       DatabaseHelperEntry();
 
-  String file; // todo
+//  int importSuccessCounter = 0;
+//  int importFailureCounter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -34,34 +33,15 @@ class _ImportState extends State<Import> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch, // max chart width
         children: <Widget>[
-          FutureBuilder(
-              future: _getDir(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done &&
-                    file != null) {
-                  return Text(file.toString());
-                } else {
-                  debugPrint(
-                      'snapshot.connectionState ${snapshot.connectionState}; file: $file');
-                  return CircularProgressIndicator(); // when Future doesn't get data
-                }
-              }),
           RaisedButton(
             color: Theme.of(context).primaryColorDark,
             textColor: Theme.of(context).primaryColorLight,
             child: Text(
-              'Import \"import_file.csv\"',
+              'Pick \"import file\"',
               textScaleFactor: 1.5,
             ),
             onPressed: () {
-              setState(() {
-                debugPrint("Import button clicked");
-                importCSVFile();
-                //debugPrint('file: $file');
-//                String exFilesDir = getExternalFilesDir();
-//                debugPrint('getExternalFilesDir(): $exFilesDir')
-                _showAlertDialog('Import status', 'status');
-              });
+              importCSVFile();
             },
           ),
         ],
@@ -69,14 +49,14 @@ class _ImportState extends State<Import> {
     ); // type lineChart
   }
 
-  String importCSVFile() {
-    String successString;
+  // IMPORT
+  void importCSVFile() async {
     int lineCounter = 0;
-    int importCounter = 0;
     List<String> attributeNames = [];
     DateTime dateTimeStamp;
 
-    final File file = new File("/storage/emulated/0/Download/import_file.csv");
+    // file picker
+    final File file = new File(await FilePicker.getFilePath());
 
     Stream<List> inputStream = file.openRead();
 
@@ -85,27 +65,22 @@ class _ImportState extends State<Import> {
         .transform(utf8.decoder) // Decode bytes to UTF-8.
         .transform(new LineSplitter()) // Convert stream to individual lines.
         .listen((String line) {
-
       List column = line.split(','); // split by comma
 
       // iterate through columns
       for (int columnCount = 0; columnCount < column.length; columnCount++) {
-
         // get attribute names
         if (lineCounter == 0) {
           attributeNames.add(column[columnCount]);
         } else {
-
           // skip empty cells in csv-file
           if ((column[columnCount]).length > 0) {
-
             // get DateTime which is in first column
             if (columnCount == 0) {
               dateTimeStamp = DateTime.parse(column[0]);
-            } else {
+
               // add entry to db
-
-
+            } else {
               // title, value, time, comment
               Entry entry = Entry(attributeNames[columnCount],
                   column[columnCount], '$dateTimeStamp', 'csv import');
@@ -118,14 +93,14 @@ class _ImportState extends State<Import> {
 
       lineCounter += 1;
     }, onDone: () {
+      _showAlertDialog('Status', 'Import started in the background');
       print('File is now closed!');
     }, onError: (e) {
       print(e.toString());
     });
-
-    return successString;
   }
 
+  // DIALOG
   void _showAlertDialog(String title, String message) {
     AlertDialog alertDialog = AlertDialog(
       title: Text(title),
@@ -134,25 +109,8 @@ class _ImportState extends State<Import> {
     showDialog(context: context, builder: (_) => alertDialog);
   }
 
-  void listDir() {
-    // Get the system temp directory.
-    var systemTempDir = Directory.current;
-
-    // List directory contents, recursing into sub-directories,
-    // but not following symbolic links.
-    systemTempDir
-        .list(recursive: true, followLinks: false)
-        .listen((FileSystemEntity entity) {
-      print(entity.path);
-    });
-  }
-
-  Future<String> _getDir() async {
-    file = await FilePicker.getFilePath();
-    return file;
-  }
-
-  void _save(entry) async {
+  // SAVE
+  Future<int> _save(entry) async {
     // Update Operation: Update a to-do object and save it to database
     int result;
     if (entry.id != null) {
@@ -164,13 +122,14 @@ class _ImportState extends State<Import> {
     }
 
     // SUCCESS FAILURE STATUS DIALOG
+    // Success
     if (result != 0) {
-      // Success
-      // TODO better feedback with result
-      _showAlertDialog('Status', 'Entry Saved Successfully');
+      //importSuccessCounter++;
     } else {
       // Failure
-      _showAlertDialog('Status', 'Problem Saving Entry');
+      _showAlertDialog('Status', 'Problem Saving Entry. Title: ${entry.title}');
+      //importFailureCounter++;
     }
+    return result;
   }
 }
