8b0dc34310000c1423131099d26959fbd5eb9d95
christianreiser <chrisi.reiser@gmail.com>
2020-08-25 13:12:59 +0200

    hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class LifeTrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: strings.appTitle,
       theme: ThemeData(
         // This is the theme of your application.
