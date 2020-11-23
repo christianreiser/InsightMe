868dc3b189bd1244070fae7e3816d3f32b4ca214
christianreiser <reisercg@gmail.com>
2020-11-23 13:21:25 +0100

    pretotype data screen

--- lib/FutureDesign/data_route.dart
+++ lib/FutureDesign/data_route.dart
@@ -5,47 +5,156 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class DataRoute extends StatelessWidget {
-  static Map<DateTime, double> dateTimeMap1 = {
-    DateTime(1): 1,
-    DateTime(2): 3,
-    DateTime(3): 3,
-    DateTime(4): 4,
-    DateTime(5): 3
-  };
-  static Map<DateTime, double> dateTimeMap2 = {
-    DateTime(1): 2,
-    DateTime(2): 5,
-    DateTime(3): 4,
-    DateTime(4): 6,
-    DateTime(5): 3
+  static Map<DateTime, double> dateTimeWeightMap = {
+    DateTime(2019, 12, 01): 74.65,
+    DateTime(2020, 01, 01): 74.6,
+    DateTime(2020, 02, 02): 74.9,
+    DateTime(2020, 03, 03): 74.8,
+    DateTime(2020, 04, 04): 75.0,
+    DateTime(2020, 05, 05): 75.2,
+    DateTime(2020, 06, 06): 75.1,
+    DateTime(2020, 07, 07): 75.2,
+    DateTime(2020, 08, 08): 75.2,
+    DateTime(2020, 09, 09): 75.3,
   };
 
-  static LineChart chart = LineChart.fromDateTimeMaps(
-      [dateTimeMap1, dateTimeMap2],
-      [Colors.green, Colors.blue],
-      ['time with friends', 'mood'], // chart label name
+  static Map<DateTime, double> dateTimeRHRMap = {
+    DateTime(2020, 09, 01): 55,
+    DateTime(2020, 09, 02): 54,
+    DateTime(2020, 09, 03): 55,
+    DateTime(2020, 09, 04): 57,
+    DateTime(2020, 09, 05): 56,
+    DateTime(2020, 09, 06): 55,
+    DateTime(2020, 09, 07): 54,
+    DateTime(2020, 09, 08): 54,
+    DateTime(2020, 09, 09): 53,
+    DateTime(2020, 09, 10): 53,
+    DateTime(2020, 09, 11): 53,
+    DateTime(2020, 09, 12): 54,
+    DateTime(2020, 09, 13): 53,
+    DateTime(2020, 09, 14): 52,
+    DateTime(2020, 09, 15): 53,
+  };
+
+  static Map<DateTime, double> dateTimeProductivityMap = {
+    DateTime(2020, 09, 01): 9,
+    DateTime(2020, 09, 02): 8,
+    DateTime(2020, 09, 03): 9,
+    DateTime(2020, 09, 04): 0,
+    DateTime(2020, 09, 05): 0,
+    DateTime(2020, 09, 06): 8,
+    DateTime(2020, 09, 07): 9,
+    DateTime(2020, 09, 08): 7,
+    DateTime(2020, 09, 09): 8,
+    DateTime(2020, 09, 10): 7,
+    DateTime(2020, 09, 11): 3,
+    DateTime(2020, 09, 12): 1,
+    DateTime(2020, 09, 13): 9,
+    DateTime(2020, 09, 14): 9,
+    DateTime(2020, 09, 15): 8,
+  };
+
+  static LineChart dateTimeWeightChart = LineChart.fromDateTimeMaps(
+      [dateTimeWeightMap], [Colors.blue], [''], // chart label name
+      tapTextFontWeight: FontWeight.w400);
+
+  static LineChart dateTimeRHRChart = LineChart.fromDateTimeMaps(
+      [dateTimeRHRMap], [Colors.blue], [''], // chart label name
+      tapTextFontWeight: FontWeight.w400);
+
+  static LineChart dateTimeProductivityChart = LineChart.fromDateTimeMaps(
+      [dateTimeProductivityMap], [Colors.blue], [''], // chart label name
       tapTextFontWeight: FontWeight.w400);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      // ChangeNotifierProvider for state management
+    return SingleChildScrollView(
       child: Column(
-          mainAxisSize: MainAxisSize.max,
-          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch, // max chart width
-          children: <Widget>[
-            //TITLE
-            Text(
-              'My Data',
-              style: TextStyle(fontSize: 27.0, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(height: 25),
-            Expanded(
-              child: AnimatedLineChart(chart),
-            ),
-          ]),
+        mainAxisSize: MainAxisSize.max,
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch, // max chart width
+        children: <Widget>[
+          // ATTRIBUTE
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                // max chart width
+                children: [
+                  Text(
+                    'Weight',
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(height: 25),
+                  SizedBox(
+                    height: 200,
+                    child: AnimatedLineChart(dateTimeWeightChart),
+                  ),
+                ]),
+          ),
+
+          // SEPARATOR
+          Container(
+            color: Colors.grey,
+            height: 1,
+          ),
+
+          //ATTRIBUTE
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                // max chart width
+                children: [
+                  Text(
+                    'Resting heart rate',
+                    style:
+                        TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(height: 25),
+                  SizedBox(
+                    height: 200,
+                    child: AnimatedLineChart(dateTimeRHRChart),
+                  ),
+                ]),
+          ),
+
+          // SEPARATOR
+          Container(
+            color: Colors.grey,
+            height: 1,
+          ),
+
+          //ATTRIBUTE
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                // max chart width
+                children: [
+                  Text(
+                    'Productive time',
+                    style:
+                    TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(height: 25),
+                  SizedBox(
+                    height: 200,
+                    child: AnimatedLineChart(dateTimeProductivityChart),
+                  ),
+                ]),
+          ),
+          // WHITE SPACE DUE TO FAB
+          SizedBox(height: 50),
+        ],
+      ),
     ); // type lineChart
   }
 }
