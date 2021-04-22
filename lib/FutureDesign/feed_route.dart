8dffbd00fc5e4e06cd0ba24aae9a52910f704396
Thanni <thanhphongtruong@live.de>
2021-04-22 15:10:30 +0200

    touch

---
+++ lib/FutureDesign/feed_route.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class FeedRoute extends StatefulWidget {
+  @override
+  _FeedRouteState createState() => _FeedRouteState();
+}
+
+class _FeedRouteState extends State<FeedRoute>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
