f59862a4626b1f85fc74f69f4a63126d61503388
Thanni <thanhphongtruong@live.de>
2021-04-27 00:39:45 +0200

    touch

--- lib/FutureDesign/feed_route.dart
+++ lib/FutureDesign/feed_route.dart
@@ -6,8 +6,10 @@ class FeedRoute extends StatefulWidget {
   _FeedRouteState createState() => _FeedRouteState();
 }
 
-class _FeedRouteState extends State<FeedRoute> with SingleTickerProviderStateMixin{
+class _FeedRouteState extends State<FeedRoute>
+    with SingleTickerProviderStateMixin {
   TabController _controller;
+  List items = getDummyList();
 
   @override
   void initState() {
@@ -17,14 +19,21 @@ class _FeedRouteState extends State<FeedRoute> with SingleTickerProviderStateMix
   }
 
   void _handleTabSelection() {
-    setState(() {
+    setState(() {});
+  }
+
+  static List getDummyList() {
+    List list = List.generate(5, (i) {
+      return "Item ${i + 1}";
     });
+    return list;
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 50.0, right: 10.0, left: 10.0), //EdgeInsets.all(10),
+      margin: const EdgeInsets.only(top: 50.0, right: 10.0, left: 10.0),
+      //EdgeInsets.all(10),
       child: Column(
         children: <Widget>[
           Row(
@@ -61,7 +70,123 @@ class _FeedRouteState extends State<FeedRoute> with SingleTickerProviderStateMix
                 ),
               ],
             ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.fastfood,
+                    ),
+                    //FlutterLogo(size: 72.0),
+                    title: Text('Energy & Food'),
+                    subtitle: Text(
+                        'To increase your energy level try eating less high temperature processed food.'),
+                    isThreeLine: true,
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.local_hospital,
+                    ),
+                    //FlutterLogo(size: 72.0),
+                    title: Text('Energy'),
+                    subtitle: Text(
+                        'Iron Deficiency. \nYour energy level correlates with iron.'),
+                    isThreeLine: true,
+                  ),
+                ),
+              ],
+            ),
           )
+
+          /* TODO: adapt this to current view
+          Container(
+            child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    key: Key(items[index]),
+                    background: Container(
+                      alignment: AlignmentDirectional.centerEnd,
+                      color: Colors.red,
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                    onDismissed: (direction) {
+                      setState(() {
+                        items.removeAt(index);
+                      });
+                    },
+                    direction: DismissDirection.endToStart,
+                    child: Card(
+                      elevation: 5,
+                      child: Container(
+                        height: 100.0,
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              height: 100.0,
+                              width: 70.0,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(5),
+                                      topLeft: Radius.circular(5)
+                                  ),
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage("https://is2-ssl.mzstatic.com/image/thumb/Video2/v4/e1/69/8b/e1698bc0-c23d-2424-40b7-527864c94a8e/pr_source.lsr/268x0w.png")
+                                  )
+                              ),
+                            ),
+                            Container(
+                              height: 100,
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      items[index],
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                                      child: Container(
+                                        width: 30,
+                                        decoration: BoxDecoration(
+                                            border: Border.all(color: Colors.teal),
+                                            borderRadius: BorderRadius.all(Radius.circular(10))
+                                        ),
+                                        child: Text("3D",textAlign: TextAlign.center,),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
+                                      child: Container(
+                                        width: 260,
+                                        child: Text("His genius finally recognized by his idol Chester",style: TextStyle(
+                                            fontSize: 15,
+                                            color: Color.fromARGB(255, 48, 48, 54)
+                                        ),),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }
+            ),
+          )
+           */
         ],
       ),
     );
