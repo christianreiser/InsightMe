04fb608c23aded9f63ee7fd454058d3f62e13b4d
christianreiser <reisercg@gmail.com>
2021-09-08 17:02:36 +0200

    replaced pld with _scaledBar

--- lib/Prediction/visualization.dart
+++ lib/Prediction/visualization.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart';
 class PredictionRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final num predictedMood = 7.0;
+    final List<double> scaleBounds = [1, 9];
+    final List<double> oneStdDev = [6, 8];
+    final List<double> twoStdDev = [5, 9];
+
     BoxDecoration _predictionBoxDecoration() {
       return BoxDecoration(
         border: Border.all(width: 1.5),
@@ -21,32 +26,49 @@ class PredictionRoute extends StatelessWidget {
       );
     }
 
-    Widget _scaledBar(start, end, scaleBounds) {
-      final int startCorrected = ((start-scaleBounds[0])*10).toInt();
+    Widget _scaledBar(start, end, scaleBounds, color, height, textBeforeBar) {
+      final int startCorrected = ((start - scaleBounds[0]) * 10).toInt();
       print('$start $end $scaleBounds');
 
-      final int endCorrected = ((end-scaleBounds[0])*10).toInt();
+      final int endCorrected = ((end - scaleBounds[0]) * 10).toInt();
       print('$startCorrected $endCorrected $scaleBounds');
       return Row(children: [
         /// empty space
-        Expanded(flex: (startCorrected), child: Container(height: 12)),
+        Expanded(
+            flex: (startCorrected),
+            child: Container(
+              alignment: Alignment.centerLeft,
+              height: height,
+              child: Row(
+                children: [
+                  SizedBox(width: 2),
+                  Text(textBeforeBar),
+                ],
+              ),
+            )),
 
         /// prediction
         Expanded(
             flex: (endCorrected - startCorrected),
-            child: Container(height: 12, color: Colors.blue)),
+            child: Container(
+              height: height,
+              color: color,
+              child: color == Colors.green
+                  ? FittedBox(child: Icon(Icons.arrow_forward_sharp))
+                  : color == Colors.red
+                      ? FittedBox(child: Icon(Icons.arrow_back_sharp))
+                      : Container(),
+            )),
 
         /// empty space
         Expanded(
           flex: ((scaleBounds[1] * 10).toInt() - 10 - endCorrected),
-          child: Container(height: 12),
+          child: Container(height: height),
         ),
       ]);
     }
 
     Container _gradientColorScale() {
-      final num predictedMood = 7.0;
-      final List<double> scaleBounds = [1, 9];
       final double startPredictedMood = predictedMood - 0.1;
       final double endPredictedMood = (predictedMood + 0.1);
       return Container(
@@ -54,47 +76,28 @@ class PredictionRoute extends StatelessWidget {
         decoration: _predictionBoxDecoration(),
         child: FractionallySizedBox(
             widthFactor: 1,
-            child:
-                /*    Row(children: [
-      /// empty space
-      Expanded(flex: (19), child: Container(height: 12)),
-
-      /// prediction interval
-      Expanded(
-          flex: (3), child: Container(height: 5, color: Colors.blue)),
-
-      /// prediction
-      Expanded(
-          flex: (1), child: Container(height: 12, color: Colors.blue)),
-
-      /// PI
-      Expanded(
-          flex: (3), child: Container(height: 5, color: Colors.blue)),
-
-      /// empty space
-      Expanded(
-        flex: (10),
-        child: Container(height: 12),
-      ),
-    ]),*/
-                _scaledBar(startPredictedMood, endPredictedMood, scaleBounds)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _scaledBar(startPredictedMood, endPredictedMood, scaleBounds,
+                    Colors.blue, 8.0, ''),
+                _scaledBar(oneStdDev[0], oneStdDev[1], scaleBounds, Colors.blue,
+                    6.0, ''),
+                _scaledBar(twoStdDev[0], twoStdDev[1], scaleBounds, Colors.blue,
+                    3.0, ''),
+              ],
+            )),
       );
     }
 
     Widget _biDirectionalGanttChart(labelNames) {
       return Column(children: [
-        Row(
-          children: [
-            Container(
-              width: 195,
-              color: Colors.black12,
-              height: 18,
-              child: Text(labelNames[0]),
-            ),
-            Container(width: 20, color: Colors.red, height: 18),
-            Container(width: 10, color: Colors.black12, height: 18),
-          ],
-        )
+        _scaledBar(5, 6.1, scaleBounds, Colors.green, 16.0, 'Mood yesterday'),
+        _scaledBar(6.1, 6.8, scaleBounds, Colors.green, 16.0, 'Humidity'),
+        _scaledBar(6.3, 6.8, scaleBounds, Colors.red, 16.0, 'Body weight'),
+        _scaledBar(6.3, 6.7, scaleBounds, Colors.green, 16.0, 'Running'),
+        _scaledBar(6.7, 7.1, scaleBounds, Colors.green, 16.0, 'Steps'),
+        _scaledBar(7.0, 7.1, scaleBounds, Colors.red, 16.0, 'CO2 level'),
       ]);
     }
 
@@ -108,22 +111,15 @@ class PredictionRoute extends StatelessWidget {
     }
 
     _numericScale() {
+      var text = new List<int>.generate(scaleBounds[1].floor().toInt(), (i) {
+        return i + 1;
+      });
       return Container(
         child: FractionallySizedBox(
           widthFactor: 1,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _scaleTile(1),
-              _scaleTile(2),
-              _scaleTile(3),
-              _scaleTile(4),
-              _scaleTile(5),
-              _scaleTile(6),
-              _scaleTile(7),
-              _scaleTile(8),
-              _scaleTile(9),
-            ],
+            children: [for (var i in text) _scaleTile(i)],
           ),
         ),
       );
@@ -145,9 +141,9 @@ class PredictionRoute extends StatelessWidget {
                 padding: const EdgeInsets.fromLTRB(1, 8, 8, 8),
                 child: Text(
                     'Mood prediction for today with confidence interval:')),
+            _biDirectionalGanttChart(labelNames),
             _gradientColorScale(),
             _numericScale(),
-            _biDirectionalGanttChart(labelNames),
           ],
         ),
       ),
